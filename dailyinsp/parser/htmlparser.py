53d28dd51942324c635d177ea93b369bdc0543f4
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-03-11 14:54:43 +0000

    added delimiter split on forward slash to get just campaign title

--- dailyinsp/parser/htmlparser.py
+++ dailyinsp/parser/htmlparser.py
@@ -28,7 +28,7 @@ class HtmlData:
                 if 'lovethework' in p.a['href']:
                     article["article_link"] = p.a['href']
             elif p.find('strong') and not p.text.endswith('.'):
-                article["article_title"] = p.text
+                article["article_title"] = p.text.split(' / ')[1]
             else:
                 article["article_desc"] = p.text
 
